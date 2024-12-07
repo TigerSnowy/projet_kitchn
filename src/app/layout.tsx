1187f022d3341aef14865cf8ee01f1943aff6b90
TigerSnowy <ines.f.ferreira.ds@gmail.com>
2024-12-07 17:17:02 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
 import { KitchnProvider } from "kitchn";
 import { KitchnRegistry } from "kitchn/next";
+import { Inter } from "next/font/google";
 
 import "kitchn/fonts.css";
 
+// Initialisation de la police Inter
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Blog avec Kitchn",
+  description: "Un blog créé avec Next.js et Kitchn",
+};
+
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
-    <html suppressHydrationWarning>
+    <html lang="fr" suppressHydrationWarning>
       <head>
-        <title>{"Create Kitchn Next"}</title>
-        <link rel={"icon"} href={"/favicon.ico"} />
+        <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>
+      <body className={inter.className}>
         <KitchnRegistry>
           <KitchnProvider>{children}</KitchnProvider>
         </KitchnRegistry>
