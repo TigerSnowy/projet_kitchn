14eb3ac66c870145f1298c1ad86e0e2eae36692c
TigerSnowy <ines.f.ferreira.ds@gmail.com>
2024-12-08 15:35:32 +0100

    spacer and icon added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Container, Text, Link, Image } from 'kitchn'
+import { Container, Text, Link, Image, Spacer, Icon } from 'kitchn'
+import { Bai_Jamjuree, Flavors } from 'next/font/google';
 
 export default function Home() {
   return (
@@ -15,8 +16,7 @@ export default function Home() {
         <Container flex={2} align={"flex-end"} justify={"center"} margin={30}>
 
         <Text size={"medium"}>
-          A statically generated blog example using <Link href={"https://nextjs.org/"}><u>{"Next.js"}</u></Link> and Markdown.
-        </Text>
+          A statically generated blog example using <Link href={"https://nextjs.org/"}><u>{"Next.js"}</u></Link> and Markdown.</Text>
 
         </Container>
 
@@ -29,25 +29,29 @@ export default function Home() {
           alt={"white stairs"}
           width={1450}
           height={"auto"}
-          // htmlWidth={540}
-          // htmlHeight={300}
+          // htmlWidth={1450}
+          // htmlHeight={"auto"}
         />
 
       </Container>
 
-      <Container row>
+      <Container row marginTop={45}>
 
-        <Container flex={1}>
+        <Container flex={1} marginLeft={33}>
 
           <Text size="title">Dynamic Routing and Static Generation</Text>
+          <Spacer y={1} />
           <Text size="medium">March 16, 2020</Text>
 
         </Container>
 
-        <Container flex={2}>
+          <Spacer x={20} />
+
+        <Container flex={2} marginRight={33}>
 
           <Text size="medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.</Text>
-          <Text weight={"extraBold"}>JJ Kasper</Text>
+          <Spacer y={1} />
+          <Text size="medium" weight={"extraBold"}><Icon src={"https://next-blog-starter.vercel.app/assets/blog/authors/jj.jpeg"} size={"extraTitle"} marginRight={15} />JJ Kasper</Text>
 
         </Container>
 
