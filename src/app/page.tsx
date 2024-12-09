59b5423353fe2537d5e9fbad0142c2256463d346
TigerSnowy <ines.f.ferreira.ds@gmail.com>
2024-12-09 01:25:38 +0100

    firt article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <Container>
 
-      <Container row>
+      <Container row marginTop={50} marginBottom={30}>
 
-        <Container flex={1} margin={30}>
+        <Container flex={1} margin={20}>
 
         <Text size={"extraTitle"} weight={"extraBold"}>Blog.</Text>
 
         </Container>
 
-        <Container flex={2} align={"flex-end"} justify={"center"} margin={30}>
+        <Container flex={2} align={"flex-end"} justify={"center"} margin={20}>
 
         <Text size={"medium"}>
           A statically generated blog example using <Link href={"https://nextjs.org/"}><u>{"Next.js"}</u></Link> and Markdown.</Text>
@@ -27,7 +27,7 @@ export default function Home() {
         <Image
           src={"https://next-blog-starter.vercel.app/_next/image?url=%2Fassets%2Fblog%2Fdynamic-routing%2Fcover.jpg&w=1920&q=75"}
           alt={"white stairs"}
-          width={1450}
+          width={1480}
           height={"auto"}
           // htmlWidth={1450}
           // htmlHeight={"auto"}
@@ -37,7 +37,7 @@ export default function Home() {
 
       <Container row marginTop={45}>
 
-        <Container flex={1} marginLeft={33}>
+        <Container flex={1} marginLeft={20}>
 
           <Text size="title">Dynamic Routing and Static Generation</Text>
           <Spacer y={1} />
@@ -45,9 +45,9 @@ export default function Home() {
 
         </Container>
 
-          <Spacer x={20} />
+          <Spacer x={25} />
 
-        <Container flex={2} marginRight={33}>
+        <Container flex={2} marginRight={25}>
 
           <Text size="medium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.</Text>
           <Spacer y={1} />
@@ -56,6 +56,32 @@ export default function Home() {
         </Container>
 
       </Container>
+
+      <Container>
+      
+        <Container flex={1} marginLeft={20}>
+
+          <Text size={"extraTitle"} weight={"extraBold"}>More Stories</Text>
+
+          <Image
+          src={"https://next-blog-starter.vercel.app/_next/image?url=%2Fassets%2Fblog%2Fhello-world%2Fcover.jpg&w=1920&q=75"}
+          alt={"white pattern"}
+          width={700}
+          height={"auto"}
+          // htmlWidth={1450}
+          // htmlHeight={"auto"}
+        />
+
+          <Text size="title">Learn How to Pre-render Pages Using Static Generation with Next.js</Text>
+          <Spacer y={1} />
+          <Text size="medium">March 16, 2020</Text>
+
+        </Container>
+
+      </Container>
+
+
+
 
     </Container>
   );
